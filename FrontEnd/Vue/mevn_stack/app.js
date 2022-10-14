ebd2c3583a8288ee8fdda513acb049066ad18758
Nithish0120 <bolt.nithish@gmail.com>
2022-10-14 20:42:25 +0530

    Middleware setup

--- FrontEnd/Vue/mevn_stack/app.js
+++ FrontEnd/Vue/mevn_stack/app.js
@@ -20,3 +20,23 @@ app.use(cors());
 
 // Setting up the Static Directory
 app.use(express.static(path.join(__dirname,'public')));
+
+
+// MongoDB config
+const db= require('./config/keys').mongoURI;
+mongoose.connect(db,{useNewUrlParser:true}).then(()=>{
+    console.log(`Succesfully connected  @ port ${db}`);
+}).catch( err => {
+    console.log(`Couldn't connect with DB ${err}`);
+});
+
+app.get('/',(req,res)=>{
+    return res.send("<h1>Hello world</h1>");
+});
+
+// App listen
+
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Server Ready At ${PORT}`);
+});
---
+++ FrontEnd/Vue/mevn_stack/config/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    mongoURI : "mongodb://localhost:27017/meven_stack",
+    secret: "MongoDB@27017"
+};
